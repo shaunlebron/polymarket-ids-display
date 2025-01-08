95a5c81e4df6c9fdbb9ba982d14fa61eabf4f34b
shaun <shaunewilliams@gmail.com>
2025-01-08 12:11:03 -0600

    reorg and use react fiber props to get condition id in game overlay

--- polymarket-ids-display.user.js
+++ polymarket-ids-display.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name        Polymarket.com display sports IDs
+// @name        Polymarket.com display market IDs
 // @match       https://polymarket.com/*
-// @description Show slug IDs for sports
+// @description add market IDs to polymarket pages
 // @namespace   com.shaunlebron.mint.js
 // @author      Shaun Williams
-// @version     1.1.0
+// @version     1.2.1
 // @homepage    https://github.com/shaunlebron/polymarket-ids-display
 // @updateURL   https://raw.githubusercontent.com/shaunlebron/polymarket-ids-display/refs/heads/main/polymarket-ids-display.user.js
 // @downloadURL https://raw.githubusercontent.com/shaunlebron/polymarket-ids-display/refs/heads/main/polymarket-ids-display.user.js
@@ -13,8 +13,6 @@
 // ==/UserScript==
 //
 
-
-// $("[data-scroll-anchor^=event-detail-accordion-item]").parentElement.__reactProps$eoxggkosow.children.map(e => e.props.market.conditionId).join(',')
 
 (function() {
 
@@ -58,6 +56,11 @@
       elem = path[i].child;
     }
     return state;
+  }
+
+  function getReactFiber(e) {
+    const k = Object.keys(e).find(k => k.startsWith("__reactFiber$"));
+    return e[k];
   }
 
   const ADDON_ID = "slug-addon"
@@ -109,46 +112,43 @@
     cond && $(card, `#${ADDON_ID} .cond`).addEventListener("click", e => { navigator.clipboard.writeText(cond); e.preventDefault(); e.stopPropagation(); });
   }
 
-  function reactProps(el) {
-    const k = Object.keys(el).filter(e => e.startsWith("__reactProps"))
-    if (k) {
-      return el[k]
+  function renderSports() {
+    // new styles cards (NBA, NFL)
+    for (const card of $$("[mode=desktop][id]")) {
+      const slug = card.id.match(/(.*)-item$/)?.[1]
+      if (slug) addIdsToCard(card, {slug, cond: slugToCond[slug]})
     }
+
+    // old style cards (EBL)
+    for (const card of $$("[data-scroll-anchor^=event-detail-accordion-item-]")) {
+      const slug = card.dataset.scrollAnchor.match(/^event-detail-accordion-item-\d+-(.*-\d{4}-\d{2}-\d{2})/)?.[1]
+      if (slug) addIdsToCard(card, {slug, cond: slugToCond[slug]})
+    }
+
+    // Game View card
+    const card = $(".sports-in-game-overlay")
+    if (card) {
+      const market = getReactFiber(card)?.pendingProps?.children?.[0]?.props?.marketsForSocial?.[0]
+      if (market) addIdsToCard(card, {slug: market.slug, cond: market.conditionId})
+    }
+  }
+
+  function renderEvent() {
+    const embedPath = 'M8.293 6.293L2.586 12 8.293 17.707 9.707 16.293 5.414 12 9.707 7.707zM15.707 17.707L21.414 12 15.707 6.293 14.293 7.707 18.586 12 14.293 16.293z';
+    const btn = $(`path[d='${embedPath}']`).closest("button")
+    const {event} = getReactProps(btn.parentElement, btn)
+    const {slug} = event
+    const cond = event.markets.map(e => e.conditionId).join(',')
+    const card = $("#event-detail-container")
+    addIdsToCard(card, {slug,cond})
   }
 
   function render() {
     const {pathname} = document.location;
-    if (pathname.startsWith("/sports")) {
-      // new styles cards (NBA, NFL)
-      for (const card of $$("[mode=desktop][id]")) {
-        const slug = card.id.match(/(.*)-item$/)?.[1]
-        if (slug) addIdsToCard(card, {slug, cond: slugToCond[slug]})
-      }
-
-      // old style cards (EBL)
-      for (const card of $$("[data-scroll-anchor^=event-detail-accordion-item-]")) {
-        const slug = card.dataset.scrollAnchor.match(/^event-detail-accordion-item-\d+-(.*-\d{4}-\d{2}-\d{2})/)?.[1]
-        if (slug) addIdsToCard(card, {slug, cond: slugToCond[slug]})
-      }
-
-      // Game View card
-      const card = $("#sports-blurred-images-background")?.parentElement
-      const scoreboard = $("#scoreboard-scroll-container")?.firstChild
-      if (card && scoreboard) {
-        const activeCardI = [...[...scoreboard.children].entries()].filter(e => e[1].children.length == 3)[0]?.[0]
-        if (activeCardI != null) {
-          const slug = reactProps(scoreboard).children.props.children[activeCardI].key
-          if (slug) addIdsToCard(card, {slug, cond: slugToCond[slug]})
-        }
-      }
+    if (pathname.startsWith("/sports/")) {
+      renderSports()
     } else if (pathname.startsWith("/event/")) {
-      const embedPath = 'M8.293 6.293L2.586 12 8.293 17.707 9.707 16.293 5.414 12 9.707 7.707zM15.707 17.707L21.414 12 15.707 6.293 14.293 7.707 18.586 12 14.293 16.293z';
-      const btn = $(`path[d='${embedPath}']`).closest("button")
-      const {event} = getReactProps(btn.parentElement, btn)
-      const {slug} = event
-      const cond = event.markets.map(e => e.conditionId).join(',')
-      const card = $("#event-detail-container")
-      addIdsToCard(card, {slug,cond})
+      renderEvent()
     }
   }
 
